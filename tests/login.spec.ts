3cf7fc424c01972fe2a4d05cd520b6b331a06ca5
Juan Torres <juantor16@gmail.com>
2025-06-21 12:03:16 -0300

    Se agregan escenarios de backend

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -17,3 +17,42 @@ test('TC-7 Verificar inicio de sesión exitoso con credenciales válidas', async
   await expect(page.getByText('Inicio de sesión exitoso')).toBeVisible();
   await expect(dashboardPage.dashboardTitle).toBeVisible();
 });
+
+test('TC-11 Loguearse con nuevo usuario creado por backend', async ({ page, request }) => {
+  const email = (TestData.usuarioValido.email.split('@')[0]) + Date.now().toString() + '@' + TestData.usuarioValido.email.split('@')[1];
+  const response = await request.post('http://localhost:6007/api/auth/signup', {
+    headers: {
+      'Accept': 'application/vnd.github.v3+json',
+      'Content-Type': 'application/json',
+    },
+    data: {
+      firstName: TestData.usuarioValido.nombre,
+      lastName: TestData.usuarioValido.apellido,
+      email: email,
+      password: TestData.usuarioValido.contraseña,
+    }
+  });
+  expect(response.status()).toBe(201);
+
+  const responsePromiseLogin = page.waitForResponse('http://localhost:6007/api/auth/login');
+  await loginPage.completarYHacerClickBotonLogin({email: email, contraseña: TestData.usuarioValido.contraseña});
+
+  const responseLogin = await responsePromiseLogin;
+  const responseBodyLoginJson = await responseLogin.json();
+
+  expect(responseLogin.status()).toBe(200);
+  expect(responseBodyLoginJson).toHaveProperty('token');
+  expect(typeof responseBodyLoginJson.token).toBe('string');
+  expect(responseBodyLoginJson).toHaveProperty('user');
+  expect(responseBodyLoginJson.user).toEqual(expect.objectContaining({
+    id: expect.any(String),
+    firstName: TestData.usuarioValido.nombre,
+    lastName: TestData.usuarioValido.apellido,
+    email: email,
+  }));
+
+
+  await expect(page.getByText('Inicio de sesión exitoso')).toBeVisible();
+  await expect(dashboardPage.dashboardTitle).toBeVisible();
+
+});
